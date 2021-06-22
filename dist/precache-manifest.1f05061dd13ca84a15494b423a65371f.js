61b48302ff4ee3a274f9660d4282be2a42dc6e83
Jeff <jeff@marmaladesoul.com>
2021-06-22 13:19:20 +1200

    Built /dist.

---
+++ dist/precache-manifest.1f05061dd13ca84a15494b423a65371f.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038f736c9cd4f3ede443a2471dc3be18",
+    "url": "assets/additionnal-page.yml.dist"
+  },
+  {
+    "revision": "359cfe1d73c2029fb10fe01b5b88267e",
+    "url": "assets/config.yml"
+  },
+  {
+    "revision": "ec485eca926557a465613a1c29f0e810",
+    "url": "assets/config.yml.dist"
+  },
+  {
+    "revision": "e4fe879a921d5f7aff3483f50719a2dd",
+    "url": "assets/config.yml.dist.sample-sui"
+  },
+  {
+    "revision": "fd699694515ebb6d8f133017dd42dbaa",
+    "url": "assets/custom.css.sample"
+  },
+  {
+    "revision": "3747a299c93ac18efb842e9c77321eae",
+    "url": "assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "3f34d7c8357fc288f219a789362384bb",
+    "url": "assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "e5da3a77211431da43847bcad73c4d28",
+    "url": "assets/icons/icon-any.png"
+  },
+  {
+    "revision": "f26b71a48b6d7b638fe480d280fa2e59",
+    "url": "assets/icons/icon-any.svg"
+  },
+  {
+    "revision": "71d635e1641b6aa7b4418e584b209117",
+    "url": "assets/icons/icon-maskable.png"
+  },
+  {
+    "revision": "733524d70dd5748e4973841831752a72",
+    "url": "assets/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "84842167f96a1ada00af60b6d3428935",
+    "url": "assets/infra-page.yml"
+  },
+  {
+    "revision": "299503dbb36a775a2f7dd2a3648e114a",
+    "url": "assets/manifest.json"
+  },
+  {
+    "revision": "2310e486d38639bd762c521ecfff5673",
+    "url": "assets/tools/sample.png"
+  },
+  {
+    "revision": "251284a7da3dbe13b758d4939254e95c",
+    "url": "assets/tools/sample2.png"
+  },
+  {
+    "revision": "ba224074cd199e43f0fa",
+    "url": "css/app.67ab7717.css"
+  },
+  {
+    "revision": "3cf7c4e1a29de1b12f7a",
+    "url": "css/chunk-3e818725.840dfbf7.css"
+  },
+  {
+    "revision": "4c04bcdf6fc03a5d67c3",
+    "url": "css/chunk-692f1e76.0ed268e5.css"
+  },
+  {
+    "revision": "bf6810b65fa27de948c2",
+    "url": "css/chunk-vendors.77fc7fd4.css"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "fonts/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "fonts/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "fonts/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "fonts/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "fonts/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "fonts/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "fonts/raleway-v14-latin-regular.43c849ea.woff2"
+  },
+  {
+    "revision": "60b344eb8dd676754364fc5ae4500d62",
+    "url": "fonts/raleway-v14-latin-regular.60b344eb.woff"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "img/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "img/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "34174d939fe6ace093cd7f5472f5c6a3",
+    "url": "index.html"
+  },
+  {
+    "revision": "ba224074cd199e43f0fa",
+    "url": "js/app.2a6718d4.js"
+  },
+  {
+    "revision": "3cf7c4e1a29de1b12f7a",
+    "url": "js/chunk-3e818725.b0843934.js"
+  },
+  {
+    "revision": "4c04bcdf6fc03a5d67c3",
+    "url": "js/chunk-692f1e76.e9757867.js"
+  },
+  {
+    "revision": "bf6810b65fa27de948c2",
+    "url": "js/chunk-vendors.72629d2e.js"
+  },
+  {
+    "revision": "471c3507cf3400229a66d6682ce45529",
+    "url": "logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
